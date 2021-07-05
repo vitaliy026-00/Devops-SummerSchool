d629c6adb7a6758a2da7e2daedbaa4980935f656
vitaliy026-00 <vitalijkovalak02@gmail.com>
2021-07-05 16:05:13 +0300

    First task

---
+++ task_owner.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+getUser=$(cat /etc/passwd | grep $1 | cut -d":" -f1)
+if [[ -z $getUser ]]; then
+echo "User not exist"
+else
+	if ! [ -d $2 ];then
+		echo "No directory"
+	else
+		if [ $UID -eq 0 ]; then
+		chown -R  $1:$1 $2
+		else echo "You're not sudo"
+		fi
+	fi 
+fi
