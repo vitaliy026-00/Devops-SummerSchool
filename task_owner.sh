953734dd5ed15f4b1c264721dcec57b5588585dd
Vitalii <72438507+vitaliy026-00@users.noreply.github.com>
2021-07-07 14:30:03 +0300

    Update task_owner.sh

--- task_owner.sh
+++ task_owner.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 getUser=$(cat /etc/passwd | grep $1 | cut -d":" -f1)
 if [[ -z $getUser ]]; then
 echo "User not exist"
@@ -8,7 +8,7 @@ else
 	else
 		if [ $UID -eq 0 ]; then
 		chown -R  $1:$1 $2
-		else echo "You're not sudo"
+		else echo "You're not root"
 		fi
 	fi 
 fi
